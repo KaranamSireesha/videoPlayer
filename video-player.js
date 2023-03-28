5d886144497c23785819c70c85fb18bf48c01843
KaranamSireesha <karanamsireesha9@gmail.com>
2023-03-28 12:22:24 +0530

    updating code

--- video-player.js
+++ video-player.js
@@ -4,65 +4,105 @@ const muteBtn = document.querySelector(".mute-btn");
 const volumeSlider = document.querySelector(".volume-slider");
 const currentTimeElement = document.querySelector(".current-time");
 const totalTimeElement = document.querySelector(".total-time");
+const progress = document.querySelector(".progress");
 const videoContainer = document.querySelector(".video-container");
 const video = document.querySelector("video");
 
-document.addEventListener("keydown", (e) => {
-  switch (e.key.toLowerCase()) {
-    case " ":
-    case "k":
-      togglePlay();
-      break;
-      case "m":
-        toggleMute();
-        break;
-  }
+// progressbar-section
+
+video.addEventListener("loadedmetadata", () => {
+  progress.setAttribute("max", video.duration);
 });
 
-//Duration-section
+video.addEventListener("timeupdate", () => {
+  progress.value = video.currentTime;
+  progress.style.width = `${Math.floor(
+    (video.currentTime * 100) / video.duration
+  )}%`;
+});
+
+progress.addEventListener("mousedown", function () {
+  video.pause();
+});
+
+progress.addEventListener("mouseup", function () {
+  video.play();
+});
+
+// //Duration-section
 
 video.addEventListener("loadeddata", () => {
-    totalTimeElement.textContent = formatDuration(video.duration)
-})
+  totalTimeElement.textContent = formatDuration(video.duration);
+});
+video.addEventListener("timeupdate", () => {
+  currentTimeElement.textContent = formatDuration(video.currentTime);
+});
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
+});
 
 function formatDuration(time) {
-    const seconds = Math.floor(time % 60);
-    const minutes = Math.floor(time / 60) % 60;
-    const hours = Math.floor(time / 3600);
+  const seconds = Math.floor(time % 60);
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600);
+  if (hours === 0) {
+    return `${minutes}:${leadingZeroFormatter.format(seconds)}`;
+  } else {
+    return `${hours}:${leadingZeroFormatter.format(
+      minutes
+    )}:${leadingZeroFormatter.format(seconds)}`;
+  }
 }
 
 // volume button
-muteBtn.addEventListener("click",toggleMute);
-volumeSlider.addEventListener("input", e => {
-    video.volume = e.target.value;
-    video.muted = e.target.value === 0;
-})
-function toggleMute(){
- video.muted = !video.muted;
+muteBtn.addEventListener("click", toggleMute);
+volumeSlider.addEventListener("input", (e) => {
+  video.volume = e.target.value;
+  video.muted = e.target.value === 0;
+});
+function toggleMute() {
+  video.muted = !video.muted;
 }
 
-video.addEventListener("volumechange", () =>{
-    volumeSlider.value = video.volume
-    let volumeLevel
-    if(video.muted || video.volume === 0 ){
-        volumeSlider.value = 0
-        volumeLevel = "muted"
-    }
-    else if(video.volume >= 0.5) {
-        volumeLevel = "high";
-      }
-    else {
-        volumeLevel = "low";
-    }
+video.addEventListener("volumechange", () => {
+  volumeSlider.value = video.volume;
+  let volumeLevel;
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0;
+    volumeLevel = "muted";
+  } else {
+    volumeLevel = "high";
+  }
 
-    videoContainer.dataset.volumeLevel = volumeLevel;
-})
+  videoContainer.dataset.volumeLevel = volumeLevel;
+});
 
 // fullscreen mode
 
-fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+if (!document?.fullscreenEnabled) {
+  fullScreenBtn.style.display = "none";
+}
+fullScreenBtn.addEventListener("click", () => {
+  fullScreenMode();
+});
+function fullScreenMode() {
+  if (document.fullscreenElement !== null) {
+    document.exitFullscreen();
+    setFullscreenData(false);
+  } else {
+    setFullscreenData(true);
+    video.requestFullscreen();
+  }
+}
 
-function toggleFullScreenMode() {}
+function setFullscreenData(state) {
+  video.setAttribute("data-fullscreen", !state);
+}
+
+document.addEventListener("fullscreenchange", () => {
+  setFullscreenData(!document.fullscreenElement);
+});
 
 // play/pause section
 
